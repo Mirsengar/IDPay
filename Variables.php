ba705ad581d08e8ee05cecf8bd82b6f392f0240c
MIRSENGAR <92634638+Mirsengar@users.noreply.github.com>
2022-06-30 02:00:51 +0430

    Add files via upload

---
+++ Variables.php
@@ -0,0 +1,7 @@
+<?php
+define('URL_CALLBACK',  'http://host/callback.php');
+define('URL_PAYMENT',   'https://api.idpay.ir/v1.1/payment');
+define('URL_INQUIRY',   'https://api.idpay.ir/v1.1/payment/inquiry');
+define('URL_VERIFY',    'https://api.idpay.ir/v1.1/payment/verify');
+define('APIKEY',        'YOUR-API-KEY');
+define('SANDBOX', FALSE);
